9a88ea3a5d95e019d3194242eef112b7b1b89609
aline <alinemunezero.920@gmail.com>
2024-11-13 18:11:56 +0200

    to find the largest number in five numbers

---
+++ 01_fundamentals/conditionals/largest.js
@@ -0,0 +1,16 @@
+var a=-5;
+var b=-2;
+var c=-6;
+var d=0;
+var e=-1;
+if(a>b && a>c && a>d && a>e){
+    console.log("the greatest is " +a);
+}else if(b>a && b>c && b>d && b>e){
+    console.log("the greatest is" +b);
+}else if(c>a && c>b && c>d && c>e){
+    console.log("the greatest is " +c)
+}else if(d>a && d>b && d>c && d>e){
+    console.log("the greatest is" +d)
+}else if(e>a && e>b && e>c && e>d){
+    console.log("the greatest is "+e)
+}
