d5854274d32c8aa2ee7283c525752cf832a4bb8d
aline <alinemunezero.920@gmail.com>
2024-11-14 19:00:57 +0200

    this was about finding the lifetime snacks i will use

---
+++ 01_fundamentals/variables/lifetime.js
@@ -0,0 +1,7 @@
+var age=22;
+var lifetime=85;
+var estimatedamount=4;
+var producttotal;
+var annualyamount=1464;
+producttotal=annualyamount*lifetime;
+console.log('I will need'+ (producttotal)+ 'to last me until the ripe old age of'+ (lifetime));
