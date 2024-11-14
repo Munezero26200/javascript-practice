e52b1244d2ed649aec0696aa78339e04dd4d3c93
aline <alinemunezero.920@gmail.com>
2024-11-14 19:27:32 +0200

    This was about converting

---
+++ 01_fundamentals/variables/converter.js
@@ -0,0 +1,6 @@
+let celsius= 90;
+let celintofernh= (celsius*9)/5 + 32;
+console.log('NN Celsius is'+ (celintofernh));
+let fahrenheit=40;
+let fahrecelsius= ((fahrenheit - 32)*5)/9;
+console.log('NN fahrenheit is'+ (fahrecelsius));
