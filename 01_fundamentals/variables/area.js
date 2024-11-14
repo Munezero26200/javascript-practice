53cc728532b86994e4460effacb067d188e72a1c
aline <alinemunezero.920@gmail.com>
2024-11-14 19:08:16 +0200

    this was about finding the circumferences and area of a circle.

---
+++ 01_fundamentals/variables/area.js
@@ -0,0 +1,8 @@
+let r=5;
+let circumference;
+let area;
+const pi=22.7;
+circumference= 2*pi*r;
+area= pi*r*r;
+console.log('The circumference is'+ (circumference));
+console.log('The area is'+ (area));
