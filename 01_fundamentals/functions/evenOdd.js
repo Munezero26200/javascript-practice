f4a9535c2201eac50481ba3fe6c4e134d998a32e
aline <alinemunezero.920@gmail.com>
2024-11-15 15:01:17 +0200

    This was about finding the even and odd number by using function

---
+++ 01_fundamentals/functions/evenOdd.js
@@ -0,0 +1,8 @@
+function isEvenOrOdd(num){
+    if(num%2===0){
+        console.log('the number is'+ 'even');
+    }else if(num%2!==0){
+        console.log('the number is'+ 'odd');
+    }
+}
+isEvenOrOdd(24);
